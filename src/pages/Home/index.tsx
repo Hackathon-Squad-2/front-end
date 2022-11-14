49124bb38afa948c57110027f11ae76496ff277b
Henrique Galieta <hgalieta@gmail.com>
2022-11-14 18:51:05 -0300

    feat(page): add information to page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,6 +3,9 @@ import { useNavigate } from 'react-router-dom';
 import { TrailCard } from '../../components/TrailCard';
 import { api } from '../../services/api';
 
+import logo from '../../assets/images/evolutionlogo.png';
+import style from './style.module.css';
+
 type Trail = {
   id: string;
   title: string;
@@ -36,17 +39,38 @@ export const Home = () => {
 
   return (
     <div>
-      <h1>Orange Evolution</h1>
-      <p>O que é a orange evolution</p>
-      {trails.map((trail) => (
-        <TrailCard
-          key={trail.id}
-          title={trail.title}
-          description={trail.description}
-          duration={trail.duration}
-          handle={() => handleInfo(trail.id)}
-        />
-      ))}
+      <img className={style.logo} src={logo} alt="Logo da Orange Evolution" />
+      <p className={style.titulo}>
+        O que é a orange evolution?
+        <br />
+        Se você:
+      </p>
+      <ul className={style.lista}>
+        <li>● Procura conteúdo tech de qualidade e gratuito;</li>
+        <li>● Está migrando de carreira para a área tech;</li>
+        <li>
+          ● Sente que com o apoio de uma comunidade vitaminada irá se
+          desenvolver mais;
+        </li>
+        <li>
+          ● Quer se preparar de forma mais assertiva para processos seletivos;
+        </li>
+        <li>● É protagonista da sua história e formação;</li>
+      </ul>
+      <p className={style.conclusao}>
+        O Orange Evolution é para você, pode chegar!
+      </p>
+      <div className={style.trilhas}>
+        {trails.map((trail) => (
+          <TrailCard
+            key={trail.id}
+            title={trail.title}
+            description={trail.description}
+            duration={trail.duration}
+            handle={() => handleInfo(trail.id)}
+          />
+        ))}
+      </div>
     </div>
   );
 };
