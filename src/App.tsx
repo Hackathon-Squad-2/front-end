1d7f0ac8d2dc8ab48b1b62ca32bb96d284781239
Marcelo Garoze <marcelogaroze@hotmail.com>
2022-11-14 13:19:33 -0300

    feat(app): added the trail :id route

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import { Home } from './pages/Home';
+import { InfoTrail } from './pages/InfoTrail';
 import { Login } from './pages/Login';
 
 import { ProtectedRoute } from './routes/ProtectedRoute';
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/trails/:id" element={<InfoTrail />} />
         <Route element={<ProtectedRoute />}></Route>
       </Routes>
     </BrowserRouter>
