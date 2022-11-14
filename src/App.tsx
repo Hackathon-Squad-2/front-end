025db65c9bfdf603e57c56e0514795fcf5baf050
Marcelo Garoze <marcelogaroze@hotmail.com>
2022-11-14 20:49:31 -0300

    feat(app): added dashboard route to the app

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/trails/:id" element={<InfoTrail />} />
+        <Route path="/trails/:id/content" element={<Dashboard />} />
         <Route path="*" element={<Navigate to="/" />} />
 
         <Route path="/dashboard" element={<Dashboard />} />
