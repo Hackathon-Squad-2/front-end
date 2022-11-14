8335a5c9988bd3a676a11faa921380b44e5c4921
Marcelo Garoze <marcelogaroze@hotmail.com>
2022-11-14 11:15:17 -0300

    feat(app): added auth provider to the project

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+import { AuthProvider } from './contexts/AuthContext';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 );
