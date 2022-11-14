72b1f1390124aa650d2d2141c32239e045f35756
Henrique Galieta <hgalieta@gmail.com>
2022-11-14 16:55:25 -0300

    chore(style): import global css

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+import './styles/global.css';
+
 import { AuthProvider } from './contexts/AuthContext';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
