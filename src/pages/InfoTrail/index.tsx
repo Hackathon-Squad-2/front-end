88a3d95b0c6c3b2c65e65e5ab7b5d567fbd2ce16
Marcelo Garoze <marcelogaroze@hotmail.com>
2022-11-14 13:21:01 -0300

    feat(pages): added info trail page

---
+++ src/pages/InfoTrail/index.tsx
@@ -0,0 +1,57 @@
+import { useEffect, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
+
+import { ContentCard } from '../../components/ContentCard';
+
+import { api } from '../../services/api';
+
+type Content = {
+  id: string;
+  title: string;
+  type: string;
+  creator: string;
+};
+
+type Trail = {
+  title: string;
+  description: string;
+  content: Content[];
+};
+
+export const InfoTrail = () => {
+  const { id } = useParams();
+
+  const [trailInfo, setTrailInfo] = useState<Trail>({} as Trail);
+
+  useEffect(() => {
+    const getContents = async () => {
+      const response = await api.get(`/trails/${id}/content`);
+
+      if (response.status !== 200) return;
+
+      setTrailInfo(response.data);
+    };
+
+    getContents();
+  }, []);
+
+  return (
+    <>
+      <h1>{trailInfo.title}</h1>
+      <p>{trailInfo.description}</p>
+      <div>
+        {trailInfo.content?.map((content) => (
+          <ContentCard
+            key={content.id}
+            title={content.title}
+            type={content.type}
+            creator={content.creator}
+          />
+        ))}
+      </div>
+      <Link to="/register">
+        <button>Quero Aprender</button>
+      </Link>
+    </>
+  );
+};
