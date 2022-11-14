85e31035338771fed3fa7360efc4e502be35e4fc
Marcelo Garoze <marcelogaroze@hotmail.com>
2022-11-14 14:15:42 -0300

    feat(app): added the register route to the app
    also added the profile route it still doesn't work with the auth context

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 
 import { Home } from './pages/Home';
 import { InfoTrail } from './pages/InfoTrail';
 import { Login } from './pages/Login';
+import { Profile } from './pages/Profile';
+import { Register } from './pages/Register';
 
 import { ProtectedRoute } from './routes/ProtectedRoute';
 
@@ -12,7 +14,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/trails/:id" element={<InfoTrail />} />
+        <Route path="*" element={<Navigate to="/" />} />
+        <Route path="/profile" element={<Profile />} />
         <Route element={<ProtectedRoute />}></Route>
       </Routes>
     </BrowserRouter>
