44ed84dae3c32e590227481d4d49337869b6c5b1
Marcelo Garoze <marcelogaroze@hotmail.com>
2022-11-14 12:09:12 -0300

    feat(pages): added the trails on the home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,50 @@
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { TrailCard } from '../../components/TrailCard';
+import { api } from '../../services/api';
+
+type Trail = {
+  id: string;
+  title: string;
+  description: string;
+  creator: string;
+  duration: number;
+};
+
 export const Home = () => {
-  return <h1>Home</h1>;
+  const [trails, setTrails] = useState<Trail[]>([]);
+
+  const navigate = useNavigate();
+
+  const handleInfo = (id: string) => {
+    // navigate(`/trails/${id}`);
+    console.log(id);
+  };
+
+  useEffect(() => {
+    const getTrails = async () => {
+      const response = await api.get<Trail[]>('/trails');
+
+      if (response.status !== 200) return;
+
+      setTrails(response.data);
+    };
+
+    getTrails();
+  }, []);
+
+  return (
+    <div>
+      {trails.map((trail) => (
+        <TrailCard
+          key={trail.id}
+          title={trail.title}
+          description={trail.description}
+          creator={trail.creator}
+          duration={trail.duration}
+          handle={() => handleInfo(trail.id)}
+        />
+      ))}
+    </div>
+  );
 };
