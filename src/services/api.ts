438063fab09919c7727e429969d5d7ceea89b0c9
Marcelo Garoze <marcelogaroze@hotmail.com>
2022-11-14 21:52:46 -0300

    feat(api): added axios interceptors

--- src/services/api.ts
+++ src/services/api.ts
@@ -5,3 +5,22 @@ const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000/api'
 export const api = axios.create({
   baseURL: API_BASE_URL,
 });
+
+interface AxiosConfig {
+  config: {
+    headers: {
+      authorization: string;
+    }
+  }
+}
+
+api.interceptors.request.use((config: any) => {
+  const token = localStorage.getItem('@hackathon:token')
+
+  if (token) {
+    // ts-noverify
+    config.headers.authorization = `Bearer ${token}`;
+  }
+
+  return config
+})
