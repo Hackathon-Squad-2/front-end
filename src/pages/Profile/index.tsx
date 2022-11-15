31416fcda32d2ac81e7def7f9f99594a80176fc7
Marcelo Garoze <marcelogaroze@hotmail.com>
2022-11-15 10:45:55 -0300

    feat(pages): finished profile for now without styles

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,17 +1,56 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { useAuth } from '../../hooks/useAuth';
 import { api } from '../../services/api';
 
+import { BiSearch } from 'react-icons/bi';
+
+type Course = {
+  id: string;
+  title: string;
+};
+
 export const Profile = () => {
+  const user = JSON.parse(localStorage.getItem('@hackathon:user')!);
+
+  const [courses, setCourses] = useState<Course[]>([]);
+
   useEffect(() => {
     const getUserInfo = async () => {
       const response = await api.get('/users/me');
 
-      console.log(response);
+      if (response.status !== 200) return;
+
+      setCourses(response.data);
     };
 
     getUserInfo();
   }, []);
 
-  return <h1>@</h1>;
+  return (
+    <>
+      <div>
+        <img src="https://via.placeholder.com/150" alt="Logo" />
+        <div>
+          <Link to="/">Home</Link>
+        </div>
+        <div>
+          <BiSearch />
+          <input type="text" />
+        </div>
+      </div>
+      <div>
+        <img src="https://via.placeholder.com/150" alt="Perfil" />
+        <span>@{user?.name}</span>
+      </div>
+      <div>
+        <span>Meus Cursos</span>
+      </div>
+      {courses.map((course) => (
+        <div key={course.id}>
+          <h2>{course.title}</h2>
+        </div>
+      ))}
+    </>
+  );
 };
